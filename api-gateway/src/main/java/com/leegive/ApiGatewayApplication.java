12cc31859815820f30c7bcce6541887902096a7c
leegive <leegive@icloud.com>
2016-10-07 01:32:15 +0800

    first commit

---
+++ api-gateway/src/main/java/com/leegive/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.leegive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringCloudApplication
+@EnableZuulProxy
+@EnableOAuth2Sso
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+}
---
+++ api-gateway/src/main/java/com/leegive/OAuthConfiguration.java
@@ -0,0 +1,13 @@
+package com.leegive;
+
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2SsoCustomConfiguration;
+
+/**
+ * Created by leegive on 07/10/2016.
+ */
+public class OAuthConfiguration {
+
+
+
+}
